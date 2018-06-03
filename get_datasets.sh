79c8584a0df9746e016e7aba11b6b6eaa359c9a4
WilliamBeverly <yuill.william1@gmail.com>
2018-06-03 15:28:52 +0200

    Updated get_dataset

--- get_datasets.sh
+++ get_datasets.sh
@@ -7,10 +7,30 @@ set -e
 # Go to the data directory and download the original dataset
 DATASET_DIR="data"
 ORIGINAL_DATASET="https://s3-us-west-1.amazonaws.com/udacity-selfdrivingcar/Lyft_Challenge/Training+Data/lyft_training_data.tar.gz"
-ORIG_DIR=""
+ORIG_DIR="lyft_training_data.tar.gz"
 
 cd "${DATASET_DIR}"
 wget "${ORIGINAL_DATASET}"
 tar -xf "${ORIG_DIR}"
+# now remove the original dataset
+rm "${ORIG_DIR}"
 
+# Go into the train directory, and download more
+TRAIN_DIR="Train"
+ADD_DATASET_1="https://www.dropbox.com/s/1etgf32uye2iy8q/world_2_w_cars.tar.gz"
+ADD_DIR_1="world_2_w_cars.tar.gz"
+ADD_DATASET_2="https://github.com/ongchinkiat/LyftPerceptionChallenge/releases/download/v0.1/carla-capture-20180528.zip"
+ADD_DIR_2="carla-capture-20180528.zip"
+cd "${TRAIN_DIR}"
+wget "${ADD_DATASET_1}"
+tar -xf "${ADD_DIR_1}"
+# now remove the original dataset
+rm "${ADD_DIR_1}"
 
+ADD_2_FOLDER='Carla_Capture'
+mkdir -p "${ADD_2_FOLDER}"
+cd "${ADD_2_FOLDER}"
+wget "${ADD_DATASET_2}"
+unzip "${ADD_DIR_2}"
+# now remove the original dataset
+rm "${ADD_DIR_2}"
